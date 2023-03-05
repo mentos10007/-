7ebf0bc588410ab86b4c14a7cde0f6071435551c
movavi_school <uvukoska@gmail.com>
2023-03-05 16:30:14 +0700

    лессон 22. 05.03.2023

---
+++ рейплейсы/lesson 22/main.py
@@ -0,0 +1,47 @@
+# # try:
+# #     number = int(input("число:"))
+# #     print(10/number)
+# # except ValueError:
+# #     print("Ы")
+# # except ZeroDivisionError:
+#
+# import json
+# f = open("file.json", "w")
+# # f.write("true")
+# peremenaya = [1, 4.3, [0, 0], (0, 0), None, True, "stroka"]
+# json.dump(peremenaya, f)
+# f.close
+#
+# f = open("file.json", "r")
+# print(f.read())
+# content = json.load
+
+# f = open("fule.txt", "r")
+# u = f.readlines()
+# print(u)
+# for i in u:
+#     p = i.split(": ")
+#     print(p)
+#
+# f = open("file.json", "r")
+# sod = json.load(f)
+# f.close()
+# print(sod)
+# for indx, elem in enumerate(sod):
+#     if type(elem) == str:
+#         sod[indx] += "!"
+#     elif type(elem) == str:
+#         sod[indx] += "!"
+#     elif type(elem) == str:
+#         sod[indx] += "!"
+#     elif type(elem) == str:
+#         sod[indx] += "!"
+
+
+
+
+
+
+
+
+import requests
