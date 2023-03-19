9c4bdb31f8835735538a540c420d87a0feb970e4
movavi_school <lavr.@yandex.ru>
2023-03-19 16:34:02 +0700

    lesson 24, 19/03/2023

---
+++ рейплейсы/23 lesson/main.py
@@ -0,0 +1,65 @@
+# # class Human:
+# #     def __init__(self):
+# #         self.height = 993
+# #
+# #
+# #
+# #     def zdarova(self):
+# #         return ""
+#
+#
+# class Car:
+#     def __init__(self):
+#     self.year = year
+#     self.type = type
+#     self.color = color
+#
+#
+# def start(self):
+#     print("машина вкл")
+#
+# def stop(self):
+#     print("машина выкл")
+#
+#
+# def year(self, year):
+# #     self.year = year
+# #
+# #
+# # def type(self, type):
+# #     self.type = type
+# #
+# #
+# # def color(self, color):
+# #     self.color = color
+#
+#
+#
+#
+#
+#
+#
+import datetime
+
+
+class Overwatch
+
+def __unit__(self):
+    self
+
+
+def chas(self):
+    pass
+
+def minute(self):
+    pass
+
+def second(self):
+    pass
+
+
+tome = Overwatch()
+print(tome.h, tome.m, tome.s)
+
+
+
