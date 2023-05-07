05a740df30dc29566a12a65b37b0c16856c6cab1
movavi_school <alexomger@mail.ru>
2023-05-07 16:35:20 +0700

    дуыыщт 31/ 07.05.2023

---
+++ рейплейсы/lesson31/main.py
@@ -0,0 +1,48 @@
+# from tkinter import *
+# root = Tk()
+# #
+# # # c = Canvas(root,)
+# #
+# # # c.create_polygon(10, 10,
+# # #                  150, 150,
+# # #                  10, 150)
+# #
+# # # c.create_oval(10, 10,
+# # #               150, 250)
+# #
+# #
+# #
+# #
+# #
+# # root.geometry("450x450")
+# #
+# # c = Canvas(root, height=450, width=450, bg="white")
+# # c.pack(anchor=CENTER)
+# # second = 0
+# #
+# # def second():
+# #     global second
+# #     second = second + 1
+# #     c.create_text(int(c["height"])/2, 450/2, text=second, front="Arial 50")
+# #
+# # root.after(1000, seconds)
+# # if secon
+# #
+# #
+# #
+# #
+# #
+#
+# def lkm(event):
+#     global l
+#     l = (event.x, event.y)
+#
+# def pkm(event):
+#     global p
+#     p = (event.x, event.y)
+#
+# btn = Button(root, text="")
+# btn.pack()
+# c.bind("<Button-1>", lkm)
+# c.bind("<Button-3>", pkm)
+# root.mainloop()
