4141915796030108c8270860ad191aab39c006e6
movavi_school <malunyarik@gmail.com>
2023-10-23 21:01:09 +0700

    zzz

---
+++ arduino/komandi.py
@@ -0,0 +1,9 @@
+# две важные штуки
+# void setup()
+# void loop()
+# 1)delay(1000) - будет работать 10 сек
+# 2)dititalWrite"пишет"/Read"читает"(13"тип", HIGH"вкл"/LOW"") -
+# 3)byte -от 0 до 256
+# 4)map (value, fromLow, from High, toLow, toHigh)
+# y = map(x, 0, 1023, 0, 255)
+# 5)
