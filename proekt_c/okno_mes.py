e57feec022ff1ea262c9694212505ca20658ed55
meqorte <meqorte1509@gmail.com>
2024-02-26 21:03:13 +0700

    okno mess nah

--- proekt_c/okno_mes.py
+++ proekt_c/okno_mes.py
@@ -7,31 +7,43 @@ from PIL import Image, ImageTk
 
 okno = tk.Tk()
 okno.geometry("1000x700")
-
+okno.title("mesag")
 
 
 # ИЗОБРАЖЕНИЕ_____________________________________________________________
-izob_polzovat = Image.open("")
-
-izob_polzovat_tk = ImageTk.PhotoImage(izob_polzovat)
-
-izob_label = tk.Label(okno, izob_polzovat=izob_polzovat_tk)
-izob_label.izob_polzovat = izob_polzovat_tk
-
-izob_label.pack()
+# izob_polzovat = Image.open("")
+#
+# izob_polzovat_tk = ImageTk.PhotoImage(izob_polzovat)
+#
+# izob_label = tk.Label(okno, izob_polzovat=izob_polzovat_tk)
+# izob_label.izob_polzovat = izob_polzovat_tk
+#
+# izob_label.pack()
 # _______________________________________________________________________
+
+
+
+lab = Label(okno, text="введите имя")
+lab.pack()
+
+
+
+# text_vod_imya = Text()
+# text_vod_imya.pack(padx=50,
+#                    pady=40
+#                    )
+
 
 
 
 
 # ВВОД ИМЯ ПОЛЬЗОВАТЕЛЯ__________________________________________________
 vod_imya = tk.Entry(okno)
-vod_imya.grid(
-                 padx=50,
-                 pady=200,
-                 row=10,
-                 column=10
-                 )
+vod_imya.pack(padx=50,
+              pady=200,
+              row=10,
+              column=10
+              )
 # _______________________________________________________________________
 
 
