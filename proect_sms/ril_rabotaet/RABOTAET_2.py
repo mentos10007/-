1385e947a28bc828b4255bbb6ebd5c485e02b5b5
John Doe <johndoe@example.com>
2024-05-13 21:17:59 +0700

    aw

--- proect_sms/ril_rabotaet/RABOTAET_2.py
+++ proect_sms/ril_rabotaet/RABOTAET_2.py
@@ -3,12 +3,21 @@ import serial
 def send_message():
     message = message_entry.get()    # Отправка сообщения на Arduino для передачи
     ser.write(message.encode())
+
+def get_message():
+    message = message_get.get()    # Получение сообщения на Arduino для передачи
+    ser.read(message.encode())
+
 root = tk.Tk()
 root.title("Мессенджер")
 message_entry = tk.Entry(root, width=50)
 message_entry.pack()
 send_button = tk.Button(root, text="Отправить", command=send_message)
 send_button.pack()
+message_get = tk.Entry(root, width=100)
+message_get.pack()
+get_button = tk.Button(root, text="Получить", command=get_message)
+get_button.pack()
 # Подключение к порту Arduino
 ser = serial.Serial('COM4', 9600)  # Укажите правильный порт
 root.mainloop()
