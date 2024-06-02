f9e277a1a883a538030e98575fd5513f3dfe7d4a
meqorte <113818073+mentos10007@users.noreply.github.com>
2024-06-02 09:31:51 +0300

    рпаи

--- proect_sms/ril_rabotaet/1.py
+++ proect_sms/ril_rabotaet/1.py
@@ -1,26 +1,15 @@
-
 import serial
-from nrf24 import NRF24
-import socket
-import struct
-import threading
+import tkinter as tk
 
-def listen(sock):
-    while True:
-        data = sock.recv(64)
-        message = struct.unpack('{}s'.format(64 - len(struct.calcsize('I'))) , data)
-        print("Received message: {}".format(message[0].decode()))
+ser = serial.Serial('COM10', 9600) # Укажите порт, к которому подключена первая Arduino
 
-ser = serial.Serial('/dev/ttyACM0', 9600)
-sock = socket.socket(socket.AF_SPI_NRF24, socket.SOCK_STREAM)
-sock.bind(NRF24.ComplexPipeline(0x30, 16))
-threading.Thread(target=listen, args=(sock,)).start()
-try:
-    while True:
-        line = ser.readline().decode().strip()
-        sock.send(struct.pack('I{}s'.format(len(line)), line.encode()))
-except KeyboardInterrupt:
-    pass
-finally:
-    sock.close()
-    ser.close()
+def send_data():
+    data = entry.get()
+    ser.write(data.encode())
+
+root = tk.Tk()
+entry = tk.Entry(root)
+entry.pack()
+button = tk.Button(root, text="Send", command=send_data)
+button.pack()
+root.mainloop()
